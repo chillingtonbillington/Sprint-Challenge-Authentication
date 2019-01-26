4b262c087f114097eb117c223df79deedccd052c
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-01-26 11:48:33 -0600

    created token generator function and wrote registration

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,40 @@
 const axios = require('axios');
-
+const knex = require('knex');
+const bcrypt = require('bcryptjs');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+const jwt = require('jsonwebtoken');
 const { authenticate } = require('./middlewares');
+
+const secret = 'I want a girl with a short skirt and a looooooooooooooooooooooooooooooooooooong secret!'
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
 };
+function tokenGenerator(username){
+  const payload ={
+    username
+  }
+  const options = {
+    expiresIn : '1h'
+  }
+  return jwt.sign(payload,secret,options)
+}
+
 
 function register(req, res) {
-  // implement user registration
+  const newUserCreds = req.body;
+  const hashedPass = bcrypt.hashSync(newUserCreds.password, 12);
+  newUserCreds.password = hashedPass;
+  db('users').insert(newUserCreds)
+    .then(ids =>{
+        res.status(201).json(ids)
+    })
+    .catch(() =>{
+      res.status(500).json({message: 'Sorry, could not register new user'})
+    })
 }
 
 function login(req, res) {
